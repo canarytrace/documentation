cde523cdbbc815492bf63df4395787d497e047b3
Radim Daniel Pánek <rdpanek@gmail.com>
2021-09-19 14:10:02 +0200

    Update lighthouse-config.js

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -4,5 +4,21 @@ module.exports = {
     maxWaitForFcp: 15 * 1000,
     maxWaitForLoad: 35 * 1000,
     formFactor: 'desktop',
-  },
+    throttling: {
+      rttMs: 40,
+      throughputKbps: 10 * 1024,
+      cpuSlowdownMultiplier: 1,
+      requestLatencyMs: 0, // 0 means unset
+      downloadThroughputKbps: 0,
+      uploadThroughputKbps: 0,
+    },
+    screenEmulation: {
+      mobile: false,
+      width: 1350,
+      height: 940,
+      deviceScaleFactor: 1,
+      disabled: false,
+    },
+    emulatedUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4590.2 Safari/537.36 Chrome-Lighthouse'
+  }
 }
