da9e868cd7ccc78914017e913240fe59da6747e1
Radim Daniel Panek <rdpanek@RDPanek.local>
2023-07-09 21:02:23 +0200

    switch md tgo mdx

---
+++ v2/doc/src/components/FeedbackFooter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function IssueFooter() {
+  return (
+    <>
+      <hr/>
+      If you find any mistakes or have any questions, please <a href="https://github.com/canarytrace/documentation/issues/new/choose">create an issue</a>. Thank you! 👍
+      <br/>
+      Do you have more questions? Please, feel free to contact us.
+    </>
+  );
+}
+
+export default IssueFooter;
