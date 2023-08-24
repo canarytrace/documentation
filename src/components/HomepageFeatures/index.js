121fe8df3b18662341a8e7c09d8393b69be7c285
Radim Daniel Pánek <rdpanek@imac.localdomain>
2023-08-24 18:48:00 +0200

    homepage textace

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -18,19 +18,19 @@ export default function HomepageFeatures() {
           </div>
           <div className="bg-gray-100 dark:bg-[#282a34] p-4 rounded-lg flex flex-col">
             <h2 className="font-sans antialiased text-xl font-semibold text-center mb-4">Canarytrace RUM</h2>
-            <ul className="list-none pl-4 text-left text-[#535569] dark:text-[#b2b5d0] space-y-2 mb-6">
-              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Odrazka 1</li>
-              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Odrazka 2</li>
-              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Odrazka 3</li>
+            <ul className="list-none pl-10 text-left text-[#535569] dark:text-[#b2b5d0] space-y-2 mb-6">
+              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Experience Web Apps User-First.</li>
+              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Effortless Monitoring; Total Security.</li>
+              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Optimize Performance, Boost Conversions</li>
             </ul>
             <Link to='/rum' className="bg-transparent text-lg text-center font-sans antialiased hover:bg-gray-300 text-gray-500 font-semibold hover:text-gray-500 py-2 px-4 border-0 rounded hover:no-underline">Get RUM</Link>
           </div>
           <div className="bg-gray-100 dark:bg-[#282a34] p-4 rounded-lg flex flex-col">
             <h2 className="font-sans antialiased text-xl font-semibold text-center mb-4">Canarytrace Listener</h2>
-            <ul className="list-none pl-4 text-left text-[#535569] dark:text-[#b2b5d0] space-y-2 mb-6">
-              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Odrazka 1</li>
-              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Odrazka 2</li>
-              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Odrazka 3</li>
+            <ul className="list-none pl-10 text-left text-[#535569] dark:text-[#b2b5d0] space-y-2 mb-6">
+              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Evaluates data from RUM and Synthetic.</li>
+              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Conducts autonomous experiments.</li>
+              <li className="relative before:content-['•'] before:absolute before:-left-5 before:text-yellow-500">Anomaly detection and alerting system.</li>
             </ul>
             <Link to='/docs/listener/introduction' className="bg-transparent text-lg text-center font-sans antialiased hover:bg-gray-300 text-gray-500 font-semibold hover:text-gray-500 py-2 px-4 border-0 rounded hover:no-underline">Get Listener</Link>
           </div>
